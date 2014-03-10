887a3e4d51b804d68cebe292a522a5fe15a58aac
Joel Christiansen <greenbadge.jc@gmail.com>
2014-03-09 23:32:52 -0600

    Finds all paragraphs under biology, strips links.

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import re, requests, praw
+import re, requests, praw, time
 from bs4 import BeautifulSoup
 
-get_paragraph = lambda: soup.find(id="Biology").next_element.next_element.next_element.prettify() #This gets the right pattern
+def get_paragraphs():
+	para = ""
+	temp = soup.find(id="Biology").next_elements#This gets the right pattern
+	for element in temp:
+		if element.name == 'p':
+			para += element.text
+		if element.name =='h2':
+			break
+	return para
 
 def fix_paragraph(body):
-	pattern = re.compile(r'(<a href="(.+?)".*?>(.*?)<\/a>)', re.DOTALL)
-	while True:
-		#[full, link, word]
-		matches = re.search(pattern, body)
-		if matches == None:
-			print "fuck you"
-			break
-		matches = matches.groups()
-		body = re.sub(matches[0], "[%s](http://bulbapedia.bulbagarden.net%s)"%(matches[2].strip(),matches[1]), body)
-	body = body.replace("\n", "")
 	body = re.sub(r"<\/{0,1}p>", "", body)
 	return body
-
 
 def get_pokemon(url):
 	global soup 
@@ -24,5 +21,5 @@ def get_pokemon(url):
 	soup = BeautifulSoup(requests.get(url).text)
 	in_soup = re.search(pattern, soup.prettify())
 	if in_soup != None:
-		return fix_paragraph(get_paragraph())
+		return fix_paragraph(get_paragraphs())
 	else: return False
